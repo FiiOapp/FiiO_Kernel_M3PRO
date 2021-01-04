cc0cee01204f0f9ac31d586eb893a702fd6baade
FiiOapp <willson@fiio.net>
2021-01-04 17:42:59 +0800

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#4 PREEMPT Wed Dec 12 15:27:20 CST 2018"
+#define LINUX_COMPILE_BY "zeke"
+#define LINUX_COMPILE_HOST "zeke-PC"
+#define LINUX_COMPILER "gcc version 4.7.2 (Ingenic 2016.11) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.14-g7b8003b1-dirty"
